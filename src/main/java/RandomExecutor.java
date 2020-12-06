f6d6e1780c18866355e7634e3bd6e49d0b4274f1
TinaTiel <tinatielwastaken@gmail.com>
2020-12-06 22:22:34 +0100

    Implemented random executor with pseudorandom cyclical behavior, closes #3

--- src/main/java/RandomExecutor.java
+++ src/main/java/RandomExecutor.java
@@ -1,13 +1,56 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class RandomExecutor implements Executor {
 
+    private List<Action> available = new ArrayList<>();
+    private List<Action> executed = new ArrayList<>();
+    private final Integer pickedPerExecution;
+    private final Random random = new Random();
 
+    public RandomExecutor(List<Action> actions, Integer pickedPerExecution) {
 
+        // Sanitize
+        if(actions == null) {
+            throw new IllegalArgumentException("actions cannot be null");
+        }
+
+        // Initialize the pool of actions to pick from
+        available.addAll(actions);
+
+        // Determine how many to pick from the pool each execution
+        if(pickedPerExecution == null || pickedPerExecution > actions.size()) {
+            this.pickedPerExecution = actions.size();
+        } else {
+            this.pickedPerExecution = pickedPerExecution;
+        }
+
+    }
 
     @Override
     public void execute(List<Action> actions, List<String> arguments) {
+
+        // sanitize inputs
+        if(actions == null || arguments == null) {
+            throw new IllegalArgumentException("actions or arguments cannot be null");
+        }
+
+        for(int i=0; i<pickedPerExecution; i++) {
+
+            // If there's nothing available, then repopulate the list
+            if(available.size() <= 0) {
+                available.addAll(executed);
+                executed.clear();
+            }
+
+            // Pick an action from what's available to execute, and execute it
+            Action picked = available.remove(random.nextInt(available.size()));
+            picked.execute(arguments);
+
+            // Add the executed action to the list of executed actions
+            executed.add(picked);
+        }
 
     }
 }
--- src/test/java/ExecutorTest.java
+++ src/test/java/ExecutorTest.java
@@ -4,32 +4,42 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
 
 public class ExecutorTest {
 
+    Action action1, action2, action3;
+    List<Action> actions = new ArrayList<>();
+    List<String> args = new ArrayList<>();
+
     @BeforeEach
     void setUp() {
+        // given actions and arguments
+        args.addAll(Arrays.asList("foo", "bar", "baz"));
 
+        action1 = mock(Action.class);
+        action2 = mock(Action.class);
+        action3 = mock(Action.class);
+
+        actions.add(action1);
+        actions.add(action2);
+        actions.add(action3);
+        assertThat(actions).hasSize(3);
     }
 
     @Test
     void executeActionsInOrder() {
 
-        // given actions
-        Action action1 = mock(Action.class);
-        Action action2 = mock(Action.class);
-        Action action3 = mock(Action.class);
-
         // When executed with arguments
-        List<String> args = Arrays.asList("foo", "bar", "baz");
         Executor executor = new InOrderExecutor(false);
-        executor.execute(Arrays.asList(action1, action2, action3), args);
+        executor.execute(actions, args);
 
         // Then the actions are executed in that order with those arguments
         InOrder inOrder = Mockito.inOrder(action1, action2, action3);
@@ -42,21 +52,97 @@ public class ExecutorTest {
     @Test
     void executeActionsInReverseOrder() {
 
-        // given actions
-        Action action1 = mock(Action.class);
-        Action action2 = mock(Action.class);
-        Action action3 = mock(Action.class);
-
         // When executed with arguments
-        List<String> args = Arrays.asList("foo", "bar", "baz");
         Executor executor = new InOrderExecutor(true);
-        executor.execute(Arrays.asList(action1, action2, action3), args);
+        executor.execute(actions, args);
 
         // Then the actions are executed in that order with those arguments
         InOrder inOrder = Mockito.inOrder(action3, action2, action1);
         inOrder.verify(action3).execute(args);
         inOrder.verify(action2).execute(args);
         inOrder.verify(action1).execute(args);
+
+    }
+
+    @Test
+    void executeRandomActionsIsPseudoRandomAndCyclical() {
+
+        // Given executor will execute two of the three
+        Executor executor = new RandomExecutor(actions, 2);
+
+        // when executed
+        executor.execute(actions, args);
+
+        // Then two of the three will be executed
+        int actionsWithOneInvocation = 0;
+        for(Action action:actions) {
+            if(Mockito.mockingDetails(action).getInvocations().size() == 1) {
+                actionsWithOneInvocation++;
+                verify(action).execute(args);
+            }
+        }
+        assertEquals(2, actionsWithOneInvocation);
+
+        // And when executed again
+        executor.execute(actions, args);
+
+        // Then at least one of them will be executed twice
+        int actionsWithTwoInvocations = 0;
+        for(Action action:actions) {
+            if(Mockito.mockingDetails(action).getInvocations().size() == 2) {
+                actionsWithTwoInvocations++;
+                verify(action, atLeastOnce()).execute(args);
+            }
+        }
+        assertEquals(1, actionsWithTwoInvocations);
+
+    }
+
+    @Test
+    void executeRandomActionsWhenNullExecutionsThenExecuteAll() {
+
+        // Given executor with null args
+        Executor executor = new RandomExecutor(actions, null);
+
+        // when executed
+        executor.execute(actions, args);
+
+        // Then all are invoked
+        verify(action1, times(1)).execute(args);
+        verify(action2, times(1)).execute(args);
+        verify(action3, times(1)).execute(args);
+
+        // And when executed again
+        executor.execute(actions, args);
+
+        // Then all are invoked again
+        verify(action1, times(2)).execute(args);
+        verify(action2, times(2)).execute(args);
+        verify(action3, times(2)).execute(args);
+
+    }
+
+    @Test
+    void executeRandomActionsWhenMoreExecutionsThanActionsThenExecuteAll() {
+
+        // Given executor with higher execution than number of actions
+        Executor executor = new RandomExecutor(actions, 999);
+
+        // when executed
+        executor.execute(actions, args);
+
+        // Then all are invoked
+        verify(action1, times(1)).execute(args);
+        verify(action2, times(1)).execute(args);
+        verify(action3, times(1)).execute(args);
+
+        // And when executed again
+        executor.execute(actions, args);
+
+        // Then all are invoked again
+        verify(action1, times(2)).execute(args);
+        verify(action2, times(2)).execute(args);
+        verify(action3, times(2)).execute(args);
 
     }
 }
