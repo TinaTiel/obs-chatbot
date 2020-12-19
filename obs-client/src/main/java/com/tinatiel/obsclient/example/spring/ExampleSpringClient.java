bfb43e08e2205260f56824999b00f7323b573c29
TinaTiel <51816935+TinaTiel@users.noreply.github.com>
2020-12-19 14:08:54 +0100

    Moved example out to separate git repo, there's no reason for it to be a submodule here. Also updated parent pom to use recent version of maven-surefire and mave-failsafe plugins so that it picks up junit5 tests.

--- obs-client/src/main/java/com/tinatiel/obsclient/example/spring/ExampleSpringClient.java
+++
@@ -1,9 +0,0 @@
-package com.tinatiel.obsclient.example.spring;
-
-import javax.websocket.Session;
-
-public interface ExampleSpringClient {
-
-    void sendSomeMessage(String yourMessage);
-    public Session getSession();
-}
