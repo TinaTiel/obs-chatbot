62054aa3b4c94fa98d8966fd2c6e19399758449e
TinaTiel <51816935+TinaTiel@users.noreply.github.com>
2020-12-22 20:03:10 +0100

    Added clarifiying docs on what the ActionSequencer is / how it is different from a scheduler/executor.

--- obschatbot/core/src/main/java/com/tinatiel/obschatbot/core/actionschedule/ActionSequencer.java
+++ obschatbot/core/src/main/java/com/tinatiel/obschatbot/core/actionschedule/ActionSequencer.java
@@ -4,6 +4,19 @@ import com.tinatiel.obschatbot.core.action.Action;
 
 import java.util.List;
 
+/**
+ * Determines the sequence of actions that will be scheduled at a later point (e.g. by an Executor/queue)
+ *
+ * Note that Actions aren't an input; this is because the ActionSequencer is stateful. For example, it may
+ * need to know what the previous sequence was to ensure all actions are sequenced eventually (such as a randomSequencer
+ * where X of Y actions are sequenced each call, but it's cyclical/pseudorandom to ensure all actions eventually
+ * are sequenced).
+ *
+ * Note also this is not called a Scheduler, as that would imply the actions would be executed at some point in
+ * time. The ActionSequencer is only responsible for determining the order of execution; scheduling should be
+ * delegated to an Executor to scheduled the actions be executed in order (but at different times depending on the
+ * time it takes to execute an Action and when the OS schedules the Thread to execute it).
+ */
 public interface ActionSequencer {
 
     List<Action> nextSequence();
