40c1706f0f00e84c64665b4d53a06f024bebe646
TinaTiel <contact@tinatiel.com>
2021-04-18 15:22:20 +0200

    FINALLY have a working test to verify that when an user approves an auth request that Spring is configured correctly to receive the response from Twitch and save the token. Holy f**King shit, that took literally a few weeks but it now works. Verified (since this test dirties the context) that it also runs as expected alongside the rest of the test suite.

--- src/test/java/com/tinatiel/obschatbot/core/client/twitch/chat/TwitchAuthTests.java
+++ src/test/java/com/tinatiel/obschatbot/core/client/twitch/chat/TwitchAuthTests.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.when;
+import static org.mockserver.model.JsonBody.json;
 
 import com.tinatiel.obschatbot.App;
 import com.tinatiel.obschatbot.core.client.twitch.auth.TwitchAuthConnectionSettings;
@@ -26,6 +27,8 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.runner.RunWith;
 import org.mockito.Answers;
 import org.mockserver.client.server.MockServerClient;
@@ -35,29 +38,18 @@ import org.mockserver.model.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
-import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizationRequestRepository;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-@DirtiesContext
-//@ContextConfiguration
+@TestInstance(Lifecycle.PER_CLASS) // so that we can have non-static BeforeAll initialization
+@DirtiesContext // Because we have to use the defined port, aaaaaa
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT, properties = {"server.port=8080"})
 public class TwitchAuthTests {
 
@@ -67,7 +59,7 @@ public class TwitchAuthTests {
   // Define our info to initialize the MockServer expectations
   private String redirectUri = "http://localhost:" + localPort + "/foo"; // doesn't matter because Spring Security uses filters to detect
   private int twitchPort = 8090;
-  private String twitchBaseUrl = "localhost:" + twitchPort;
+  private String twitchBaseUrl = "http://localhost:" + twitchPort;
   private String twitchAuthPath = "/somepath/authorize";
   private String twitchTokenPath = "/somepath/token";
   private List<String> scopes = Arrays.asList("foo:bar");
@@ -90,13 +82,13 @@ public class TwitchAuthTests {
   @Autowired
   OAuth2AuthorizedClientService authorizedClientService;
 
-  @BeforeClass
+  @BeforeAll
   public void beforeClass() {
     mockServer = ClientAndServer.startClientAndServer(twitchPort);
     createTokenExchangeExpectations();
   }
 
-  @AfterClass
+  @AfterAll
   public void afterClass() {
     mockServer.stop();
   }
@@ -105,7 +97,7 @@ public class TwitchAuthTests {
   void setUp() {
     when(twitchAuthConnectionSettingsFactory.getSettings()).thenReturn(
       TwitchAuthConnectionSettings.builder()
-        .host("http://" + twitchBaseUrl)
+        .host(twitchBaseUrl)
         .authorizationPath(twitchAuthPath)
         .tokenPath(twitchTokenPath)
         .scopes(scopes)
@@ -123,12 +115,11 @@ public class TwitchAuthTests {
     HashMap<String, Object> attributes = new HashMap<>();
     attributes.put("registration_id", "twitch");
     OAuth2AuthorizationRequest authorizationRequest = OAuth2AuthorizationRequest.authorizationCode()
-      .authorizationUri("http://" + twitchBaseUrl + twitchAuthPath)
+      .authorizationUri(twitchBaseUrl + twitchAuthPath)
       .clientId(clientId)
       .redirectUri(redirectUri)
       .scopes(new HashSet<>(scopes))
       .state(state)
-//      .authorizationRequestUri() // need? https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=drk7vknesfqclm67yywgo15qkxzvij&scope=channel:moderate%20chat:read%20chat:edit&state=x0OKi7RPbzg8g6IIRkg7JwzVbL5lSqR_-s8vAf8A-D8%3D&redirect_uri=http://localhost:8080/foo/bar
       .attributes(attributes)
       .build();
     when(authorizationRequestRepository.loadAuthorizationRequest(any()))
@@ -164,6 +155,8 @@ public class TwitchAuthTests {
 
     assertThat(authorizedClient).isNotNull();
     assertThat(authorizedClient.getAccessToken().getTokenValue()).isEqualTo(accessToken);
+    assertThat(authorizedClient.getRefreshToken().getTokenValue()).isEqualTo(refreshToken);
+//    assertThat(authorizedClient.getAccessToken().getScopes()).containsExactlyInAnyOrderElementsOf(scopes); // TODO create modified HttpMessageConverter; Spring expects a String, not a JSON array
 
   }
 
@@ -177,14 +170,18 @@ public class TwitchAuthTests {
       .respond(
         HttpResponse.response()
           .withStatusCode(200)
-          .withBody("{" + System.lineSeparator()
-            + "\"access_token\": \"" + accessToken + "\"," + System.lineSeparator()
-            + "\"expires_in\": " + expiresIn + "," + System.lineSeparator()
-            + "\"refresh_token\": \"" + refreshToken + "\"," + System.lineSeparator()
-            + "\"scope\": [" + scopes.stream().map(it -> "\"" + it + "\"").collect(Collectors.joining(",")) + "]," + System.lineSeparator()
-            + "\"token_type\": \"" + "bearer" + "\"," + System.lineSeparator()
-            + "}"
-          )
+          .withHeader("Content-Type", "application/json")
+          .withBody(json(createTokenJson(accessToken, expiresIn, refreshToken, scopes)))
       );
+  }
+
+  String createTokenJson(String accessToken, int expiresIn, String refreshToken, List<String> scopes) {
+    return "{" + System.lineSeparator()
+      + "\"access_token\": \"" + accessToken + "\"," + System.lineSeparator()
+      + "\"expires_in\": " + expiresIn + "," + System.lineSeparator()
+      + "\"refresh_token\": \"" + refreshToken + "\"," + System.lineSeparator()
+      + "\"scope\": [" + scopes.stream().map(it -> "\"" + it + "\"").collect(Collectors.joining(",")) + "]," + System.lineSeparator()
+      + "\"token_type\": \"" + "bearer" + "\"" + System.lineSeparator()
+      + "}";
   }
 }
